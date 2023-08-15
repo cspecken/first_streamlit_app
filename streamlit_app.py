c006cc7e088ca610fc414cffa723af20ca07c790
cspecken <cspecken@gmail.com>
2023-08-14 23:00:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,10 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+add_my_fruit streamlit.text_input('What fruit would you like add?','Jackfruit')
+streamlit.write ('Thanks for adding ', add_my_fruit)
+
+#This will not work correctly 
+my_cur.execute("insert into fruit_load_list values  ('from streamlit')")
